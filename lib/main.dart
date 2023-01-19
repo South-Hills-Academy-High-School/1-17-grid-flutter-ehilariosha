eded105cd12786e0af4c252586a5bd7a67ca85a6
Erick <ehilariostudent@shacademy.org>
2023-01-19 14:24:51 -0800

    working on app

---
+++ lib/main.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => MyAppState(),
+      child: MaterialApp(
+        title: '1-17 App',
+        theme: ThemeData(),
+        home: MyHomePage(),
+      ),
+    );
+  }
+}
+
+class MyAppState extends ChangeNotifier {
+  var opacityList = [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0,];
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+    return Scaffold(
+      body: GridView.builder(
+        itemCount: 9,
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 3,
+        ),
+        itemBuilder: (context, index) {
+          return Opacity(
+            opacity: 0.5,
+            child: Padding(
+              padding: const EdgeInsets.all(8),
+              child: SizedBox(
+                  height: 50,
+                  width: 50,
+                child: Material(
+                  color: Colors.blueGrey[700],
+                  child: InkWell(
+                    child: Image.network(
+                        'https://media1.giphy.com/media/3oz9ZE2Oo9zRC/100.webp?cid=ecf05e47omc0kf5g5pv4zwe52dvmftzqqs0f44xotq4w6pn7&rid=100.webp&ct=g'),
+                        onTap: () {},
+                  ),
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
